1e29c6c9fd955f5b095f0cdd8db835424da19260
Siddharth Rao <siddrao.926@gmail.com>
2024-06-11 19:45:10 -0700

    Added Trained Model File Loading

--- src/build_model.py
+++ src/build_model.py
@@ -1,20 +1,29 @@
 from keras.api.models import Sequential
 from keras.api.layers import Input, Dense, Conv2D, MaxPooling2D, Flatten
+from keras.api.models import load_model
+
+import os
 
 
 def _build_model():
 
-    model = Sequential([
-        Input(shape=(512, 512, 3)),
-        Conv2D(32, (3, 3), activation='relu'),
-        MaxPooling2D((2, 2)),
-        Conv2D(64, (3, 3), activation='relu'),
-        MaxPooling2D((2, 2)),
-        Conv2D(64, (3, 3), activation='relu'),
-        Flatten(),
-        Dense(64, activation='relu'),
-        Dense(27, activation='softmax')
-    ])
+    if os.path.exists('model.keras'):
+        print('Model already exists. Loading model.')
+        model = load_model('model.keras')
+
+    else:
+
+        model = Sequential([
+            Input(shape=(512, 512, 3)),
+            Conv2D(32, (3, 3), activation='relu'),
+            MaxPooling2D((2, 2)),
+            Conv2D(64, (3, 3), activation='relu'),
+            MaxPooling2D((2, 2)),
+            Conv2D(64, (3, 3), activation='relu'),
+            Flatten(),
+            Dense(64, activation='relu'),
+            Dense(27, activation='softmax')
+        ])
 
     model.compile(
         optimizer='adam',
