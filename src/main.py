132b7e87b5b7ae1479702a7deb6b05fb66cce877
Siddharth Rao <siddrao.926@gmail.com>
2024-06-11 19:34:10 -0700

    Added Scripts To Main Function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
-from load_data import loadData
+from load_data import getData
+from build_model import build_model
 
 
 def main():
-    loadData()
+    train_data, test_data = getData()
+
+    model = build_model()
+
+    model.fit(train_data, epochs=10, batch_size=32)
+
+    model.evaluate(test_data)
+
+    model.save('model.h5')
 
 
 if __name__ == "__main__":
